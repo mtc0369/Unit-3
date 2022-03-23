f47f60debd3a3ec3952e15fbbccd0383c2d23943
mtc0369 <mtconnolly3@wisc.edu>
2022-03-23 13:36:19 -0500

    Boilerplate
    Boilerplate

---
+++ boilerplate/js/main.js
@@ -0,0 +1,10 @@
+// Add all scripts to the JS folder
+function myFunc() {
+    var mydiv = document.getElementById("mydiv");
+    mydiv.innerHTML = "Hello World";
+};
+
+window.onload = myFunc();
+
+
+
